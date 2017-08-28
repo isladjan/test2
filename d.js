38c31b91895b752de8f6108945a151c5bc11afa1
Sladjan Ilic <isladjan83@gmail.com>
2017-08-28 14:03:40 +0200

    mala izmena, iztumbao sam malo redove u d fajlu

--- d.js
+++ d.js
@@ -1,8 +1,9 @@
-//ovo je prva izmena ovde
 <script>
    window.addEventListener('message', msgHandler, false);
    function msgHandler(e) {
 	  document.getElementsByTagName('h1')[0].innerHTML = e.data;
    }
 </script>
+//ovo je prva izmena ovde
+  
 //ovo je izmena koja postoji samo u d2 branch
