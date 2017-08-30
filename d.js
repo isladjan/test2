16b498ae720ed95e79e797d5154cb98c10487f43
Sladjan Ilic <isladjan83@gmail.com>
2017-08-30 11:28:58 +0200

    ...

--- d.js
+++ d.js
@@ -1,1 +1,9 @@
+<script>
+   window.addEventListener('message', msgHandler, false);
+   function msgHandler(e) {
+	  document.getElementsByTagName('h1')[0].innerHTML = e.data;
+   }
+</script>
 //ovo je prva izmena ovde
+  
+//ovo je izmena koja postoji samo u d2 branch
